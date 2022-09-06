36c71afb1710a511d8b3c8810203a68b6d5fb877
Michael <michael@duplessis.nz>
2022-09-06 13:47:36 +1200

    basic framework of django project

---
+++ first_project/first_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from first_app import views
+
+urlpatterns = [
+    path('', views.index, name=''),
+    path('theView', views.alternative, name=''),
+
+]
--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from first_app import views
 
 urlpatterns = [
-    path('', views.index, name=''),
-    path('theView', views.alternative, name=''),
+
+    path('', include('first_app.urls')),
+    # path('', views.alternative, name=''),
+
     path('admin/', admin.site.urls),
 
 ]
