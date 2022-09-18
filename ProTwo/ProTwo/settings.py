8d3062caefb9c368d590ae0accdcbd71ca0e344d
Michael <michael@duplessis.nz>
2022-09-18 19:38:47 +1200

    add all the project files

--- ProTwo/ProTwo/settings.py
+++ ProTwo/ProTwo/settings.py
@@ -123,7 +123,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR, ]
 
-print (STATICFILES_DIRS)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
